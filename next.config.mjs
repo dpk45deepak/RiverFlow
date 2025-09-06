071d11a467cfb55425e6aa78fdbdb770d4ec37b1
dpk.41deepak <dpk.41deepak@gmail.com>
2025-09-06 14:41:08 +0000

    deploy on netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    distDir: 'build',   // default is `.next`, but now it will be `build/`
+  };
+  
+  export default nextConfig;
+  
